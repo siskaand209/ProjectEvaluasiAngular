f7f93b04cfda8578eaa7e09f7f13a49c0e3d5d58
drajat2610 <drajat2610@gmail.com>
2018-04-12 16:38:31 +0700

    m_roles created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './page/home/home.component';
+import { RolesComponent } from './page/roles/roles.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'roles', component: RolesComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
+export const routingComponents = [HomeComponent, RolesComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,36 @@ import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
-
+import { AppRoutingModule } from './app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Http, HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { RolesComponent } from './page/roles/roles.component';
+import { RoleService } from './services/role.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RolesComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpModule,
+    ReactiveFormsModule,
+    FormsModule,
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [ {provide: APP_BASE_HREF, useValue: '/'}, RoleService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
