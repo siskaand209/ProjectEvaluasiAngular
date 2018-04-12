3d310b0a643d66058efd6ab0d9544b4fc87dad0c
sptclr <sipitacular@gmail.com>
2018-04-12 15:05:55 +0700

    menu access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from './page/home/home.component';
+import { MenuaccessComponent } from './page/menuaccess/menuaccess.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'menuaccess', component: MenuaccessComponent}
+]
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   declarations: []
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+             HomeComponent,
+             MenuaccessComponent
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
-
 import { AppComponent } from './app.component';
-import { EmployeesComponent } from './page/employee/employees/employees.component';
+import { routingComponents, AppRoutingModule } from './app-routing.module';
+import { HttpClient } from 'selenium-webdriver/http';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Http, HttpModule } from '@angular/http';
+
+
+import { EmployeesComponent } from './page/employees/employees.component';
+import { HomeComponent } from './page/home/home.component';
+import { MenuaccessComponent } from './page/menuaccess/menuaccess.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    HomeComponent,
+    MenuaccessComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpModule,
+    ReactiveFormsModule,
+    FormsModule,
     BsDropdownModule.forRoot(),
-    TooltipModule.forRoot(),
     ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [
+      {provide: APP_BASE_HREF, useValue: '/'} 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
